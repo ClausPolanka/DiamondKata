64a7303f548270569683d959c71fe13f94606016
Claus Polanka <claus.polanka@chello.at>
2015-04-18 22:11:18 +0200

    [Green] Implemented a walking skeleton for input 'A'.

--- test/domain_tests/Diamond_Builder_Test.cs
+++ test/domain_tests/Diamond_Builder_Test.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using domain;
 using Xunit;
 
 namespace domain_tests
@@ -16,14 +17,6 @@ namespace domain_tests
             var sut = new Diamond_Builder();
             var actual_diamond = sut.Build_Diamond(middle_letter: input);
             Assert.Equal(expected_diamond, actual_diamond);
-        }
-    }
-
-    public class Diamond_Builder
-    {
-        public string Build_Diamond(string middle_letter)
-        {
-            return string.Empty;
         }
     }
 }
