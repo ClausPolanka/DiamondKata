a9e970a643964052d03feec011f7b310f01c5260
Claus Polanka <claus.polanka@chello.at>
2015-04-18 22:24:18 +0200

    [Red] Finished all only insert spaces functionality is missing 'B'.

--- src/domain/Diamond_Builder.cs
+++ src/domain/Diamond_Builder.cs
@@ -26,20 +26,30 @@ namespace domain
         {
             var index_of_last_letter = alphabet.IndexOf(last_letter);
             var letters = alphabet.Take(count: index_of_last_letter + 1).ToList();
-            return letters;
+            var correct_nr_of_letters = letters
+                .Where(l => l != "A")
+                .Select(l => l + l)
+                .ToList();
+            correct_nr_of_letters.Insert(0, "A");
+            return correct_nr_of_letters;
         }
 
         private List<string> Insert_Spaces(List<string> letters)
         {
+            // TODO Build skeleton.
             return letters;
         }
 
-        private List<string> Duplicate_First_Half_of_Diamond(List<string> half_diamond)
+        private IEnumerable<string> Duplicate_First_Half_of_Diamond(List<string> half_diamond)
         {
+            var duplicate = half_diamond
+                .Take(count: half_diamond.Count - 1)
+                .ToList();
+            half_diamond.AddRange(duplicate);
             return half_diamond;
         }
 
-        private string To_String(List<string> diamond)
+        private string To_String(IEnumerable<string> diamond)
         {
             return string.Join(separator: NEW_LINE, values: diamond);
         }
